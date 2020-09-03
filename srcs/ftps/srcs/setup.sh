61d1c0e05c9919c4045201dbdfbd59f25de6a21f
Thijs Bruineman <tbruinem@pop-os.localdomain>
2020-09-03 17:23:56 +0200

    cleanup

--- srcs/ftps/srcs/setup.sh
+++ srcs/ftps/srcs/setup.sh
@@ -1,3 +1,6 @@
+adduser -D -h /var/ftp $FTPS_USER && \
+echo "$FTPS_USER:$FTPS_PASS" | chpasswd
+
 . /tmp/get_external-ip-address.sh PASV_ADDR ftps-svc
 envsubst '${PASV_ADDR} ${MYSQL_DB_HOST}' < /tmp/vsftpd.conf > etc/vsftpd/vsftpd.conf
 
