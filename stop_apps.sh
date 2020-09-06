2ad230792a9a0d227de3f796d26d22ca774b7f93
Thijs Bruineman <tbruinem@pop-os.localdomain>
2020-09-06 14:04:49 +0200

    added stop_apps

--- stop_apps.sh
+++ stop_apps.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-kubectl delete pods --all
-kubectl delete deployments --all
-kubectl delete configmaps config -n metallb-system
-kubectl delete pvc --all
-kubectl delete svc ftps
-kubectl delete svc mysql
-kubectl delete svc nginx-svc
-kubectl delete svc phpmyadmin-svc
-kubectl delete svc wordpress-svc
-kubectl delete svc telegraf-svc
-kubectl delete svc grafana-svc
-kubectl delete svc influxdb-svc
-
-eval $(minikube docker-env -u)
+kubectl delete svc,deployments,pv,pvc,pods,configmaps,role,rolebinding,secret,serviceaccount \
+	-l tier=general --wait=false
+kubectl delete svc,deployments,pv,pvc,pods,configmaps,role,rolebinding,secret,serviceaccount \
+	-l app=nginx --wait=false
+kubectl delete svc,deployments,pv,pvc,pods,configmaps,role,rolebinding,secret,serviceaccount \
+	-l app=ftps --wait=false
+kubectl delete svc,deployments,pv,pvc,pods,configmaps,role,rolebinding,secret,serviceaccount \
+	-l app=mysql --wait=false
+kubectl delete svc,deployments,pv,pvc,pods,configmaps,role,rolebinding,secret,serviceaccount \
+	-l app=wordpress --wait=false
+kubectl delete svc,deployments,pv,pvc,pods,configmaps,role,rolebinding,secret,serviceaccount \
+	-l app=phpmyadmin --wait=false
+kubectl delete svc,deployments,pv,pvc,pods,configmaps,role,rolebinding,secret,serviceaccount \
+	-l app=influxdb --wait=false
+kubectl delete svc,deployments,pv,pvc,pods,configmaps,role,rolebinding,secret,serviceaccount \
+	-l app=telegraf --wait=false
+kubectl delete svc,deployments,pv,pvc,pods,configmaps,role,rolebinding,secret,serviceaccount \
+	-l app=grafana --wait=false
